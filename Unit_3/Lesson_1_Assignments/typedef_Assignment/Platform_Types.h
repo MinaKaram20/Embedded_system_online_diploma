3f9769e561a58edc85591c7508d55f63e09e40ae
MinaKaram20 <minakaram205@gmail.com>
2024-08-15 15:38:06 +0300

    Editing Platform_types

--- Unit_3/Lesson_1_Assignments/typedef_Assignment/Platform_Types.h
+++ Unit_3/Lesson_1_Assignments/typedef_Assignment/Platform_Types.h
@@ -8,19 +8,19 @@
 #ifndef PLATFORM_TYPES_H_
 #define PLATFORM_TYPES_H_
 
-typedef unsigned long			 boolean;
+typedef unsigned long			 boolean_t;
 
-typedef unsigned char			 uint8;
-typedef unsigned short	 	 	 uint16;
-typedef unsigned long	 		 uint32;
-typedef unsigned long long		 uint64;
-typedef signed char			 	 sint8;
-typedef signed short	 		 sint16;
-typedef signed long				 sint32;
-typedef signed long long		 sint64;
+typedef unsigned char			 uint8_t;
+typedef unsigned short	 	 	 uint16_t;
+typedef unsigned long	 		 uint32_t;
+typedef unsigned long long		 uint64_t;
+typedef signed char			 	 sint8_t;
+typedef signed short	 		 sint16_t;
+typedef signed long				 sint32_t;
+typedef signed long long		 sint64_t;
 
-typedef float 					 float32;
-typedef double					 float64;
+typedef float 					 float32_t;
+typedef double					 float64_t;
 
 
 
